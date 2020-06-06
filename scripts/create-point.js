2cb2b1dcfc5a823bc1fb159d757d51eba2052e87
Andre Luiz Da Silva Chiapetta <andreluizchiapetta@gmail.com>
2020-06-05 23:39:49 -0300

    Dia 03

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -50,10 +50,32 @@ document
         item.addEventListener("click", handleSelectedItem)
     }
 
+    const collectedItems = document.querySelector("input[name=items]")
+
+    let selectedItems = [];
+
     function handleSelectedItem(event) {
         const itemLi = event.target
 
         itemLi.classList.toggle("selected")
 
         const itemId = event.target.dataset.id
+
+        const alreadySelected = selectedItems.findIndex( item => {
+            const itemFound = item == itemId
+            return itemFound
+        })
+
+        if ( alreadySelected >= 0 ) {
+            const filteredItems = selectedItems.filter( item => {
+                const itemIsDifferent = item != itemId
+                return itemIsDifferent
+            })
+
+            selectedItems = filteredItems
+        } else {
+            selectedItems.push(itemId)
+        }
+
+        collectedItems.value = selectedItems
     }
