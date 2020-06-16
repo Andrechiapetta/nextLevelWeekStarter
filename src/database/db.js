cf93956f8a1fc1bd115f90bf8199b646a83f7fc6
Andre Luiz Da Silva Chiapetta <andreluizchiapetta@gmail.com>
2020-06-15 23:12:24 -0300

    Dia 05

--- src/database/db.js
+++ src/database/db.js
@@ -50,21 +50,21 @@ module.exports = db
 
     db.run(query, values, afterInsertData)
 
-    //db.all(`SELECT name FROM places`, function(err, rows) {
-    //    if(err) {
-    //        return console.log(err)
-    //    }
+    db.all(`SELECT name FROM places`, function(err, rows) {
+        if(err) {
+            return console.log(err)
+        }
 
-    //    console.log("Aqui estão seus registros: ")
-    //    console.log(rows)
-   // })
+        console.log("Aqui estão seus registros: ")
+        console.log(rows)
+    })
 
-    //db.run(`DELETE FROM places WHERE id = ?`, [1], function(err) {
-    //    if(err) {
-    //        return console.log(err)
-    //    }
+    db.run(`DELETE FROM places WHERE id = ?`, [1], function(err) {
+        if(err) {
+            return console.log(err)
+        }
 
-    //    console.log("Registro deletado com sucesso!")
-    //})
+        console.log("Registro deletado com sucesso!")
+    })
 
 }) */
