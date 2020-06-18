be9bf407d05d9fa4e0dcc37b64ccfbd448eafd18
Andre Luiz Da Silva Chiapetta <andreluizchiapetta@gmail.com>
2020-06-17 23:52:57 -0300

    Dia 05

--- src/server.js
+++ src/server.js
@@ -20,6 +20,16 @@ server.get("/create-point", (req, res) => {
 })
 
 server.get("/search", (req, res) => {
+
+    db.all(`SELECT name FROM places`, function(err, rows) {
+        if(err) {
+            return console.log(err)
+        }
+
+        console.log("Aqui estão seus registros: ")
+        console.log(rows)
+    })
+
     return res.render("search-results.html")
 })
 
